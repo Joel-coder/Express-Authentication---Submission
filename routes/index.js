381e85161e17c2c2424bbca83b3b8c050448877b
Joel Vargas <jvargasa@my.centennialcollege.ca>
2021-10-13 00:12:27 -0400

    first commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var router = express.Router();
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Welcome!" });
+});
+
+/* GET home page. */
+router.get("/home", function (req, res, next) {
+  res.render("index", { title: "Welcome to my webpage!" });
+});
+
+/* GET About Us page. */
+router.get("/about", function (req, res, next) {
+  res.render("about", { title: "About" });
+});
+
+/* GET project page. */
+router.get("/project", function (req, res, next) {
+  res.render("project", { title: "Project" });
+});
+
+/* GET Services page. */
+router.get("/services", function (req, res, next) {
+  res.render("services", { title: "Services" });
+});
+
+/* GET Contact Us page. */
+router.get("/contact", function (req, res, next) {
+  res.render("contact", { title: "Contact" });
+});
+
+module.exports = router;
