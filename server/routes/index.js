53fe95a88f881e29cc6df34578befa159a1c7d1a
Joel Vargas <joelvargasapolinario@gmail.com>
2021-10-24 11:32:26 -0400

    a couple more ingredients

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,7 +1,7 @@
 let express = require("express");
 let router = express.Router();
 
-let indexController = require("../controller/index")
+let indexController = require("../controller/index");
 
 /* GET home page. */
 router.get("/", indexController.displayHomePage);
@@ -19,5 +19,20 @@ router.get("/services", indexController.displayServicesPage);
 
 /* GET Contact Us page. */
 router.get("/contact", indexController.displayContactPage);
+
+/* GET Router for displaying the Login page */
+router.get("/login", indexController.displayLoginPage);
+
+/* POST Router for processing the Login Page */
+router.post("/login", indexController.processLoginPage);
+
+/* GET Router for displaying the register page */
+router.get("/register", indexController.displayRegisterPage);
+
+/* POST Router for processing the register Page */
+router.post("/register", indexController.processRegisterPage);
+
+/* GET Router for perform log out */
+router.get("/logout", indexController.performLogout);
 
 module.exports = router;
