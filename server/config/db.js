8be40d7c3b46328cc15a116ae51380f6ce52774b
Joel Vargas <joelvargasapolinario@gmail.com>
2021-10-24 17:50:08 -0400

    finally

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,7 @@
 module.exports = {
-  URI: "mongodb://localhost:27017/contacts_list",
+  // URI: "mongodb://localhost:27017/contacts_list",
+  URI: "mongodb+srv://jvargas:cfSNBxtflvARkF8U@mongodbserver.36qg1.mongodb.net/contacts_list?retryWrites=true&w=majority",
+
   Secret: "SomeSecret",
   // URI: "mongodb://localhost:27017/portfolio",
 };
